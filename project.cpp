cf1d9dca0ec4723ebfd6bac5b202cd9fb3ecdfe6
Ali Haider <90311252+Amoremeoo@users.noreply.github.com>
2024-12-04 19:40:10 +0500

    Inizialization
    This code is the initial structure for a Smart Study Scheduler program in C. It allows users to input the total number of hours they plan to study and the number of subjects they want to study.

--- project.cpp
+++ project.cpp
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    int numSubjects;
+    int total_hours;
+    char subjects[MAX_SUBJECTS][50];
+    int hours[MAX_SUBJECTS];
+    int level[MAX_SUBJECTS];
+    printf("====================== SMART STUDY SCHEDULAR ======================\n\n");
+    printf("\n\nEnter The Total Hours you Wish to study for:");
+    scanf("%d", &total_hours);
+    printf("\n\nEnter the total  number of subjects you are going to study:");
+    scanf("%d", &numSubjects);
+    if (numSubjects > MAX_SUBJECTS)
+    {
+        printf("\n\nYou can only enter up to %d subjects.\n", MAX_SUBJECTS);
+        return 1;
+    }
+
+
+
+
+    
 
     return 0;
 }
